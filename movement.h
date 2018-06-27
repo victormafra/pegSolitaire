ff6fc052993a4dba4dca75f444cf54add513c271
Victor Lucas de Melo Mafra <vlmmafra@gmail.com>
2018-06-27 20:36:55 -0300

    First commit

---
+++ movement.h
@@ -0,0 +1,194 @@
+#ifndef _movement_h
+#define _movement_h
+
+#include <iostream>
+using namespace std;
+
+#define clear() printf("\033[H\033[J")
+
+void printBoard(char (&board)[7][7]){
+    cout << "  ";
+    for(int i=0; i<7; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+            if(j==0){
+                cout << i << " ";
+            }
+            cout << board[i][j] << " ";
+        }
+        cout << i << endl;
+    }
+    cout << "  ";
+    for(int i=0; i<7; i++){
+        cout << i << " ";
+    }
+    cout << endl << endl;
+}
+
+int score(char (&board)[7][7]){
+    int sum = 0;
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+            if(board[i][j] == '1'){
+                sum++;
+            }
+        }
+    }
+    return sum;
+}
+
+bool inRange( char (&board)[7][7], int x, int y){
+    if (x>=7 || x<0 || y>=7 || y<0 || board[x][y]=='#')
+        return false;
+    return true;
+}
+
+void initializeBoard( char (&board)[7][7] ){
+    string line;
+    ifstream myfile("tab.txt");
+
+    int i=0;
+    if(myfile.is_open()){
+        while( getline(myfile, line) ){
+            if(i<7){
+                for(int j=0; j<7; j++){
+                    board[i][j] = line[j*2];
+                }
+                ++i;
+            }
+        }
+        myfile.close();
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+
+}
+
+bool moveIsValid(char (&board)[7][7], string pos, int x, int y){
+    int xKill, xNewPos, yKill, yNewPos;
+    xKill = xNewPos = yKill = yNewPos = 0;
+
+    if(pos=="up"){
+        xKill = x-1;
+        yKill = y;
+        xNewPos = x-2;
+        yNewPos = y;
+    } else if(pos=="down"){
+        xKill = x+1;
+        yKill = y;
+        xNewPos = x+2;
+        yNewPos = y;
+    } else if(pos=="left"){
+        xKill = x;
+        yKill = y-1;
+        xNewPos = x;
+        yNewPos = y-2;
+    } else if(pos=="right"){
+        xKill = x;
+        yKill = y+1;
+        xNewPos = x;
+        yNewPos = y+2;
+    }
+
+    if(board[x][y] == '1' && board[xKill][yKill] == '1' && board[xNewPos][yNewPos] == '0'
+    && inRange(board, x,y) && inRange(board, xKill,yKill) && inRange(board, xNewPos,yNewPos))
+        return true;
+    return false;
+
+}
+
+bool moveIsValid(char (&board)[7][7], int x, int xKill, int xNewPos, int y, int yKill, int yNewPos){
+
+    if(board[x][y] == '1' && board[xKill][yKill] == '1' && board[xNewPos][yNewPos] == '0'
+    && inRange(board, x,y) && inRange(board, xKill,yKill) && inRange(board, xNewPos,yNewPos))
+        return true;
+    return false;
+
+}
+
+struct movement {
+    int x;
+    int y;
+    string direction;
+};
+
+int checkPossibleMoves(char (&board)[7][7], bool print){
+    string possibleMoves[] = {"up", "down", "left", "right"};
+    vector <movement> moves;
+    movement tempMove;
+    int moveCount[] = {0, 0, 0, 0};
+
+    int sum = 0;
+
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+
+            for(int k=0; k<4; k++){
+                if( moveIsValid(board, possibleMoves[k], i, j) ){
+                    tempMove.x = i;
+                    tempMove.y = j;
+                    tempMove.direction = possibleMoves[k];
+                    moves.push_back(tempMove);
+                    moveCount[k]++;
+                    sum++;
+                }
+            }
+
+        }
+    }
+
+    if(sum>0 && print){
+        cout << "Total de possíveis movimentos: " << sum << endl;
+        for(int k=0; k<4; k++){
+            if(moveCount[k]>0){
+                cout << possibleMoves[k] << ": " << moveCount[k] << endl;
+            }
+        }
+    }
+    return sum;
+
+}
+
+
+bool move(char (&board)[7][7], string pos, int x, int y){
+    int xKill, xNewPos, yKill, yNewPos;
+    xKill = xNewPos = yKill = yNewPos = 0;
+
+    if(pos=="up"){
+        xKill = x-1;
+        yKill = y;
+        xNewPos = x-2;
+        yNewPos = y;
+    } else if(pos=="down"){
+        xKill = x+1;
+        yKill = y;
+        xNewPos = x+2;
+        yNewPos = y;
+    } else if(pos=="left"){
+        xKill = x;
+        yKill = y-1;
+        xNewPos = x;
+        yNewPos = y-2;
+    } else if(pos=="right"){
+        xKill = x;
+        yKill = y+1;
+        xNewPos = x;
+        yNewPos = y+2;
+    }
+
+    if(moveIsValid(board, x,xKill,xNewPos,y,yKill,yNewPos)){
+        cout << "Made "<< pos <<" (" << x << "," << y << ") to (" << xNewPos << "," << yNewPos << ");" << endl;
+        board[x][y] = '0';
+        board[xKill][yKill] = '0';
+        board[xNewPos][yNewPos] = '1';
+        return 1;
+    }
+    return 0;
+
+}
+
+#endif
---
+++ peg.cpp
@@ -0,0 +1,173 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include "movement.h"
+using namespace std;
+
+struct boards{
+    char board[7][7];
+};
+
+vector <vector<boards> > chronos;
+
+bool equalBoards(char (&board)[7][7], char (&newBoard)[7][7]){
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+            if ( newBoard[i][j]!=board[i][j] ) {
+                return false;
+            }
+
+        }
+    }
+    return true;
+}
+
+bool isBoardHere(char (&board)[7][7], vector <boards> vet){
+    for(int i=0; i<vet.size(); i++){
+        if (equalBoards(board,vet[i].board))
+            return true;
+    }
+    return false;
+}
+
+
+bool condition(movement a, movement b){
+    string possibleMoves[] = {"up", "down", "left", "right"};
+    for(int i=0; i<4; i++){
+        if(a.direction==possibleMoves[i])
+            return 1;
+        if(b.direction==possibleMoves[i])
+            return 0;
+    }
+}
+
+vector <movement> possibleMoves(char (&board)[7][7]){
+    string possibleMoves[] = {"up", "down", "left", "right"};
+    vector <movement> moves;
+    movement tempMove;
+    int moveCount[] = {0, 0, 0, 0};
+
+    int sum = 0;
+
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+
+            for(int k=0; k<4; k++){
+                if( moveIsValid(board, possibleMoves[k], i, j) ){
+                    tempMove.x = i;
+                    tempMove.y = j;
+                    tempMove.direction = possibleMoves[k];
+                    moves.push_back(tempMove);
+                    moveCount[k]++;
+                    sum++;
+                }
+            }
+
+        }
+    }
+
+    return moves;
+}
+
+void copyBoard(char (&board)[7][7], char (&newBoard)[7][7]){
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+            newBoard[i][j]=board[i][j];
+        }
+    }
+}
+
+bool makeMove(char (&board)[7][7], char (&newBoard)[7][7], string pos, int x, int y){
+    copyBoard(board,newBoard);
+
+    int xKill, xNewPos, yKill, yNewPos;
+    xKill = xNewPos = yKill = yNewPos = 0;
+
+    if(pos=="up"){
+        xKill = x-1;
+        yKill = y;
+        xNewPos = x-2;
+        yNewPos = y;
+    } else if(pos=="down"){
+        xKill = x+1;
+        yKill = y;
+        xNewPos = x+2;
+        yNewPos = y;
+    } else if(pos=="left"){
+        xKill = x;
+        yKill = y-1;
+        xNewPos = x;
+        yNewPos = y-2;
+    } else if(pos=="right"){
+        xKill = x;
+        yKill = y+1;
+        xNewPos = x;
+        yNewPos = y+2;
+    }
+
+    if(moveIsValid(board, x,xKill,xNewPos,y,yKill,yNewPos)){
+        newBoard[x][y] = '0';
+        newBoard[xKill][yKill] = '0';
+        newBoard[xNewPos][yNewPos] = '1';
+        return 1;
+    }
+    return 0;
+}
+
+boards tempBoard;
+bool zabalaponga = false;
+int contadorAtemporal = 0;
+void ergia(char (&board)[7][7]){
+    char newBoard[7][7];
+
+    vector <movement> moves = possibleMoves(board);
+
+    if(moves.size()==0){
+        if(score(board)==1){
+            cout << "Possible score: " << score(board) << endl;
+            printBoard(board);
+            zabalaponga = true;
+        }
+    } else {
+        for(int i=0; i<moves.size() && !zabalaponga; i++){
+            if(makeMove(board, newBoard, moves[i].direction ,moves[i].x, moves[i].y)){
+                if(!isBoardHere(newBoard,chronos[score(board)-1])){
+                    copyBoard(newBoard,tempBoard.board);
+                    chronos[score(board)-1].push_back(tempBoard);
+                    contadorAtemporal++;
+                    ergia(newBoard);
+                }
+            }
+        }
+    }
+
+}
+
+void kindaDumb(char (&board)[7][7]){
+    string possibleMoves[] = {"up", "down", "left", "right"};
+    while (checkPossibleMoves(board, 0) ) {
+        for(int i=0; i<7; i++){
+            for(int j=0; j<7; j++){
+                for(int k=0; k<4; k++){
+                    move(board, possibleMoves[k], i, j);
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    clear();
+    chronos.resize(35);
+    char board[7][7];
+    cout << "Start with: " << endl;
+    initializeBoard(board);
+    printBoard(board);
+
+    vector <pair<int, int> > movementsMade;
+    ergia(board);
+
+    return 0;
+}
